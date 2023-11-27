9ac66b9b23583b1de5cd18a333e7a340f913423b
b4kii <dami4nhalibart@gmail.com>
2023-11-27 20:54:10 +0100

    update gitignore

--- app/routes.php
+++ app/routes.php
@@ -4,4 +4,3 @@ declare(strict_types=1);
 use App\Controllers\HomeController;
 
 $router->get("/", [HomeController::class, "index"])->middleware("guest");
-$router->get("/show", [HomeController::class, "show"])->middleware("guest");
