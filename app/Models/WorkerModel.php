3f600b7d8b80dbc8fae4cdf034ec1cf6bf4ba522
PatrykB <p.boguslawski@netland.pl>
2024-01-25 23:17:42 +0100

    Update worker model

--- app/Models/WorkerModel.php
+++ app/Models/WorkerModel.php
@@ -18,4 +18,19 @@ class WorkerModel extends BaseModel
     {
         return $this->addRecord("Address", $data);
     }
+
+    public function updateClient($data, $id)
+    {
+        return $this->updateRecords("Client", $data, "Id = {$id}");
+    }
+
+    public function updateAddress($data, $id)
+    {
+        return $this->updateRecords("Address", $data, "Id = {$id}");
+    }
+
+    public function updateCar($data, $id)
+    {
+        return $this->updateRecords("Car", $data, "Id = {$id}");
+    }
 }
