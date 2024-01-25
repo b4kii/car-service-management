65be153af8b8038e96e457d7db07cbfb96dafe6d
PatrykB <p.boguslawski@netland.pl>
2024-01-25 23:17:33 +0100

    Update worker controller

--- app/Controllers/WorkerController.php
+++ app/Controllers/WorkerController.php
@@ -30,6 +30,40 @@ class WorkerController
         ]);
     }
 
+    public function updateClientIndex()
+    {
+        $id = $_GET['id'];
+
+        $clientModel = $this->model->getById("Client", $id);
+        if(!$clientModel) {
+            return $this->twig->render('errors/404.html.twig');
+        }
+
+        $addressModel = $this->model->getById("Address", $clientModel['AddressId']);
+        if(!$addressModel) {
+            return $this->twig->render('errors/404.html.twig');
+        }
+
+        return $this->twig->render('worker/update/update-client.html.twig', [
+            "clientModel" => $clientModel,
+            "addressModel" => $addressModel
+        ]);
+    }
+
+    public function updateCarIndex()
+    {
+        $id = $_GET['id'];
+
+        $carModel = $this->model->getById("Car", $id);
+        if(!$carModel) {
+            return $this->twig->render('errors/404.html.twig');
+        }
+
+        return $this->twig->render('worker/update/update-car.html.twig', [
+            "carModel" => $carModel
+        ]);
+    }
+
     public function addClient()
     {
         $validator = new Validator($_POST);
@@ -120,7 +154,7 @@ class WorkerController
             ],
             "lengthBetween" => [
                 ["model", 1, 20],
-                ["identificationNumber", 1, 15],
+                ["identificationNumber", 1, 20],
                 ["color", 1, 15],
                 ["mileage", 1, 20],
                 ["engineCapacity", 1, 20],
@@ -170,5 +204,143 @@ class WorkerController
         ];
 
         $this->model->addCar($carData);
+    }
+
+    public function updateClient()
+    {
+        $validator = new Validator($_POST);
+        $validator->rules([
+            "required" => [
+                ["firstname"],
+                ["lastname"],
+                ["city"],
+                ["street"],
+                ["phone"],
+                ["email"],
+            ],
+            "lengthBetween" => [
+                ["firstname", 1, 50],
+                ["lastname", 1, 50],
+                ["NIP", 1, 15],
+                ["city", 1, 20],
+                ["street", 1, 20],
+                ["phone", 1, 15],
+            ],
+            "email" => [
+                ["email"]
+            ]
+        ]);
+
+        // client basic data
+        $clientId = $_POST["clientId"];
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $nip = $_POST["NIP"];
+        $type = $_POST["type"];
+
+        // client address data
+        $addressId = $_POST["addressId"];
+        $city = $_POST["city"];
+        $street = $_POST["street"];
+        $postCode = $_POST["postCode"];
+        $houseNumber = $_POST["houseNumber"];
+        $phone = $_POST["phone"];
+        $email = $_POST["email"];
+
+        if (!$validator->validate())
+        {
+            Session::flash("errors", [
+                "firstname" => $validator->errors("firstname"),
+                "lastname" => $validator->errors("lastname"),
+                "city" => $validator->errors("city"),
+                "street" => $validator->errors("street"),
+                "phone" => $validator->errors("phone"),
+                "email" => $validator->errors("email"),
+            ]);
+
+            redirect("/update-client", "?id={$clientId}");
+        }
+
+        $addressData = [
+            'City' => $city,
+            'Street' => $street,
+            'PostCode' => $postCode,
+            'HouseNumber' => $houseNumber,
+            'Phone' => $phone,
+            'Email' => $email
+        ];
+        $this->model->updateAddress($addressData, $addressId);
+
+        $clientData = [
+            'Firstname' => $firstname,
+            'Lastname' => $lastname,
+            'NIP' => $nip,
+            'Type' => $type
+        ];
+        $this->model->updateClient($clientData, $clientId);
+    }
+
+    public function updateCar()
+    {
+        $validator = new Validator($_POST);
+        $validator->rules([
+            "required" => [
+                ["model"],
+                ["identificationNumber"],
+                ["color"],
+                ["mileage"],
+                ["engineCapacity"],
+                ["submissionDate"],
+            ],
+            "lengthBetween" => [
+                ["model", 1, 20],
+                ["identificationNumber", 1, 20],
+                ["color", 1, 15],
+                ["mileage", 1, 20],
+                ["engineCapacity", 1, 20],
+            ],
+        ]);
+
+        $carId = $_POST["carId"];
+        $brand = $_POST["brand"];
+        $model = $_POST["model"];
+        $identificationNumber = $_POST["identificationNumber"];
+        $type = $_POST["type"];
+        $color = $_POST["color"];
+        $status = $_POST["status"];
+        $mileage = $_POST["mileage"];
+        $engineCapacity = $_POST["engineCapacity"];
+        $submissionDate = $_POST["submissionDate"];
+
+        if (!$validator->validate())
+        {
+            Session::flash("errors", [
+                "brand" => $validator->errors("brand"),
+                "model" => $validator->errors("model"),
+                "identificationNumber" => $validator->errors("identificationNumber"),
+                "clientId" => $validator->errors("clientId"),
+                "type" => $validator->errors("type"),
+                "color" => $validator->errors("color"),
+                "mileage" => $validator->errors("mileage"),
+                "engineCapacity" => $validator->errors("engineCapacity"),
+                "submissionDate" => $validator->errors("submissionDate"),
+            ]);
+
+            redirect("/update-car", "?id={$carId}");
+        }
+
+        $carData = [
+            'brand' => $brand,
+            'model' => $model,
+            'identificationNumber' => $identificationNumber,
+            'color' => $color,
+            'mileage' => $mileage,
+            'engineCapacity' => $engineCapacity,
+            'type' => $type,
+            'status' => $status,
+            'submissionDate' => $submissionDate
+        ];
+
+        $this->model->updateCar($carData, $carId);
     }
 }
